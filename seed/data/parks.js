c87bf5d71256ca5aa1f6dbfe652362e8d6fa3ac6
slaterJJ <jackslater@hotmail.com>
2017-01-24 09:44:01 +0000

    Add park info to Alexandra park

--- seed/data/parks.js
+++ seed/data/parks.js
@@ -5,7 +5,11 @@ module.exports = [{
     latitude: 53.451562,
     longitude: -2.249320
   },
-  active: true
+  active: true,
+  quoteTitle: 'The People\'s Park',
+  quote: 'I do not forget that it is a park of the people. The people have made it; the people have paid for it; the people will use it.',
+  quoteAuthor: 'The Mayor of Manchester, Alexandra Park Opening, 1870',
+  info: 'The Park was opened to the public in 1870 and was designed by Alexander Hennell. The design of the Park was experimental in its use of oval shaped and curved pathways, which contrasted with the more rigid geometry seen previously in Victorian landscapes. It is located between the vibrant communities of Whalley Range & Moss Side, just off Princess Road and covers 60 acres.'
 },
 {
   name: 'Platt Fields Park',
