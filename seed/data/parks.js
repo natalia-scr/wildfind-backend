f7f144fa90d7099c85d22eefd8cdd897debf2d22
slaterJJ <jackslater@hotmail.com>
2017-01-26 19:11:59 +0000

    Add images to the parks

--- seed/data/parks.js
+++ seed/data/parks.js
@@ -9,7 +9,8 @@ module.exports = [{
   quoteTitle: 'The People\'s Park',
   quote: 'I do not forget that it is a park of the people. The people have made it; the people have paid for it; the people will use it.',
   quoteAuthor: 'The Mayor of Manchester, Alexandra Park Opening, 1870',
-  info: 'The Park was opened to the public in 1870 and was designed by Alexander Hennell. The design of the Park was experimental in its use of oval shaped and curved pathways, which contrasted with the more rigid geometry seen previously in Victorian landscapes. It is located between the vibrant communities of Whalley Range & Moss Side, just off Princess Road and covers 60 acres.'
+  info: 'The Park was opened to the public in 1870 and was designed by Alexander Hennell. The design of the Park was experimental in its use of oval shaped and curved pathways, which contrasted with the more rigid geometry seen previously in Victorian landscapes. It is located between the vibrant communities of Whalley Range & Moss Side, just off Princess Road and covers 60 acres.',
+  img: 'https://c2.staticflickr.com/4/3364/3581344330_ed3ba5a631_z.jpg?zz=1'
 },
 {
   name: 'Platt Fields Park',
@@ -18,7 +19,8 @@ module.exports = [{
     latitude: 53.451562,
     longitude: -2.249320
   },
-  active: false
+  active: false,
+  img: 'http://s2.evcdn.com/images/edpborder500/I0-001/002/446/933-2.jpeg_/platt-fields-park-33.jpeg'
 },
 {
   name: 'Heaton Park',
@@ -27,7 +29,8 @@ module.exports = [{
     latitude: 53.501566,
     longitude: -2.195458
   },
-  active: false
+  active: false,
+  img: 'https://media-cdn.tripadvisor.com/media/photo-s/07/c4/b3/a3/heaton-park.jpg'
 },
 {
   name: 'Tatton Park',
@@ -36,7 +39,8 @@ module.exports = [{
     latitude: 53.501566,
     longitude: -2.195458
   },
-  active: false
+  active: false,
+  img: 'http://www.playingwithflowers.co.uk/wp-content/uploads/2012/05/Trine-Messenger-and-Pont-de-Singe-Tatton-Park-Biennial-20122.jpg'
 },
 {
   name: 'Wythenshawe Park',
@@ -45,5 +49,6 @@ module.exports = [{
     latitude: 53.501566,
     longitude: -2.195458
   },
-  active: false
+  active: false,
+  img: 'http://i3.manchestereveningnews.co.uk/incoming/article639712.ece/ALTERNATES/s615/C_71_article_1469306_long_teaser_group_long_teaser_image.jpg'
 }];
